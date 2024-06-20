733a23c0b9d937f4bd2568a7f526bd4da2927c06
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-06-20 00:16:57 -0700

    apparently, betty just as my best interest at heart

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/* Using printf to print the message with proper grammar */
+	printf("%s, but the outcome is a piece of art,\n", "Programming is like building a multilingual puzzle");
+	
+	return (0);
+}
