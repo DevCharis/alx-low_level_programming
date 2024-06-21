68b77dee49afef39b5a2e6efd74cd69323136039
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-06-21 05:59:07 -0700

    whats tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the lowercase alphabet in reverse, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char letter = 'z';
+
+    /* Loop to print alphabet in reverse */
+    while (letter >= 'a')
+    {
+        putchar(letter);
+        letter--;
+    }
+
+    putchar('\n'); /* Print new line */
+
+    return (0); /* Indicate successful termination */
+}
