3e0229ae7bdba5eeeb80aecb3c9b97c48a754350
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-06-21 06:42:42 -0700

    okay, to be fair, its not betty this time around

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,66 +10,30 @@
  */
 int main(void)
 {
-    int num1_hundreds = 0;
-    int num1_tens = 0;
-    int num1_units = 0;
-    int num2_hundreds = 0;
-    int num2_tens = 0;
-    int num2_units = 1;
+    int num1, num2;
 
-    while (num1_hundreds <= 9)
+    num1 = 0;
+
+    while (num1 <= 99)
     {
-        while (num1_tens <= 9)
+        num2 = num1 + 1;
+        while (num2 <= 99)
         {
-            while (num1_units <= 9)
-            {
-                while (num2_hundreds <= 9)
-                {
-                    while (num2_tens <= 9)
-                    {
-                        while (num2_units <= 9)
-                        {
-                            if ((num1_hundreds < num2_hundreds) ||
-                                (num1_hundreds == num2_hundreds &&
-                                 num1_tens < num2_tens) ||
-                                (num1_hundreds == num2_hundreds &&
-                                 num1_tens == num2_tens &&
-                                 num1_units < num2_units))
-                            {
-                                putchar(num1_hundreds + '0');
-                                putchar(num1_tens + '0');
-                                putchar(num1_units + '0');
-                                putchar(' ');
-                                putchar(num2_hundreds + '0');
-                                putchar(num2_tens + '0');
-                                putchar(num2_units + '0');
+            putchar(num1 / 10 + '0');  /* Tens digit of num1 */
+            putchar(num1 % 10 + '0');  /* Units digit of num1 */
+            putchar(' ');
+            putchar(num2 / 10 + '0');  /* Tens digit of num2 */
+            putchar(num2 % 10 + '0');  /* Units digit of num2 */
 
-                                if (!(num1_hundreds == 9 &&
-                                      num1_tens == 8 &&
-                                      num1_units == 9 &&
-                                      num2_hundreds == 9 &&
-                                      num2_tens == 9 &&
-                                      num2_units == 9))
-                                {
-                                    putchar(',');
-                                    putchar(' ');
-                                }
-                            }
-                            num2_units++;
-                        }
-                        num2_tens++;
-                        num2_units = 0;
-                    }
-                    num2_hundreds++;
-                    num2_tens = num1_tens + 1;
-                    num2_units = 0;
-                }
-                num1_units++;
+            if (!(num1 == 98 && num2 == 99))  /* Don't print comma and space after last combination */
+            {
+                putchar(',');
+                putchar(' ');
             }
-            num1_tens++;
-            num1_units = 0;
+
+            num2++;
         }
-        num1_hundreds++;
+        num1++;
     }
 
     putchar('\n');
