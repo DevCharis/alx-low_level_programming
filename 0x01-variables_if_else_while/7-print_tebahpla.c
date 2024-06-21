57009076846a88598d7275bea313eaca03d47ff2
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-06-21 06:11:13 -0700

    betty is after my life but i refuse to accept

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,16 +9,16 @@
  */
 int main(void)
 {
-    char letter = 'z';
+char letter = 'z';
 
     /* Loop to print alphabet in reverse */
-    while (letter >= 'a')
-    {
-        putchar(letter);
-        letter--;
-    }
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
+}
 
-    putchar('\n'); /* Print new line */
+putchar('\n'); /* Print new line */
 
-    return (0); /* Indicate successful termination */
+return (0); /* Indicate successful termination */
 }
