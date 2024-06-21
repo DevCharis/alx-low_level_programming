41c6df5b2c0471fa9e168d43a93bc666a6f6c28d
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-06-21 06:49:19 -0700

    okay, to be fair, its not betty this time around

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,32 +10,31 @@
  */
 int main(void)
 {
-    int num1, num2;
+int num1, num2;
 
-    num1 = 0;
+num1 = 0;
 
-    while (num1 <= 99)
-    {
-        num2 = num1 + 1;
-        while (num2 <= 99)
-        {
-            putchar(num1 / 10 + '0');  /* Tens digit of num1 */
-            putchar(num1 % 10 + '0');  /* Units digit of num1 */
-            putchar(' ');
-            putchar(num2 / 10 + '0');  /* Tens digit of num2 */
-            putchar(num2 % 10 + '0');  /* Units digit of num2 */
+while (num1 <= 99)
+{
+num2 = num1 + 1;
+while (num2 <= 99)
+{
+putchar(num1 / 10 + '0');  /* Tens digit of num1 */
+putchar(num1 % 10 + '0');  /* Units digit of num1 */
+putchar(' ');
+putchar(num2 / 10 + '0');  /* Tens digit of num2 */
+putchar(num2 % 10 + '0');  /* Units digit of num2 */
 
-            if (!(num1 == 98 && num2 == 99))  /* Don't print comma and space after last combination */
-            {
-                putchar(',');
-                putchar(' ');
-            }
+if (!(num1 == 98 && num2 == 99))  /* Don't print comma and space after last combination */
+{
+putchar(',');
+putchar(' ');
+}
+num2++;
+}
+num1++;
+}
 
-            num2++;
-        }
-        num1++;
-    }
-
-    putchar('\n');
-    return (0);
+putchar('\n');
+return (0);
 }
