fa0494d98929a7f1d62ce2d5985bf2d6055251bd
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-06-25 12:39:12 -0700

    betty, havent you done enough

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,17 +5,16 @@
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    char ch;
-
-    for (i = 0; i < 10; i++)
-    {
-        ch = 'a';
-        while (ch <= 'z')
-        {
-            _putchar(ch);
-            ch++;
-        }
-        _putchar('\n');
-    }
+int i;
+char ch;
+for (i = 0; i < 10; i++)
+{
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+}
 }
