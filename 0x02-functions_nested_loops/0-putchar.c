51695fc98ad42355b53f94611b429bff7b535d86
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-06-25 11:37:20 -0700

    today is about putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_puts("_putchar\n");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+I guess we working with the ifs, whiles and such
