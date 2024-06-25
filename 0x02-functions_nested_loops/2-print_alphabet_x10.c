228e91db04182ffa162d20ee41a3ea66bdf7d6ad
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-06-25 12:27:51 -0700

    this is giving me a headache

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,20 @@
 
 /**
  * print_alphabet_x10 - Prints the alphabet 10 times in lowercase
- *
- * Description: Uses nested loops to achieve the desired output.
  */
 void print_alphabet_x10(void)
 {
-    int i, ch;
+    int i;
+    char ch;
 
     for (i = 0; i < 10; i++)
     {
-        for (ch = 'a'; ch <= 'z'; ch++)
+        ch = 'a';
+        while (ch <= 'z')
         {
-            _putchar(ch); // First use of _putchar
+            _putchar(ch);
+            ch++;
         }
-        _putchar('\n'); // Second use of _putchar
+        _putchar('\n');
     }
 }
