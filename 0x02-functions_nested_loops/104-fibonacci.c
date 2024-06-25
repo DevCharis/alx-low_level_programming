900efc3f717d878806ebdbb9f44862d25fb22d9a
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-06-25 15:08:55 -0700

    fib giving me its own share of problems

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,22 +7,20 @@
  */
 int main(void)
 {
-unsigned long fib1 = 1, fib2 = 2, next, sum = 2;
+unsigned long fib1 = 1, fib2 = 2, next;
 int count;
 
-printf("%lu", fib1);
-printf(", %lu", fib2);
+printf("%lu, %lu", fib1, fib2);
 
 for (count = 3; count <= 98; count++)
 {
 next = fib1 + fib2;
-if (next % 2 == 0)
-sum += next;
 printf(", %lu", next);
 fib1 = fib2;
 fib2 = next;
 }
 
-printf("\n%lu\n", sum);
+printf("\n");
+
 return (0);
 }
