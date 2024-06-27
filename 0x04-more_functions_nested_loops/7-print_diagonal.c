ffff66e40f869587178023a8299abe19e9d3d59f
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-06-27 10:22:06 -0700

    trying that two birds thing again

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,9 +3,9 @@
 /**
 * print_diagonal - draws a diagonal line on the terminal
 * @n: number of times the character '\' should be printed
-* 
 * Description: If n is 0 or less, the function prints only '\n'. Otherwise,
-* it prints a diagonal line of length n using '\' characters followed by a newline.
+* it prints a diagonal line of length n using
+*'\' characters followed by a newline.
 */
 void print_diagonal(int n)
 {
