fadaa957c6e1bc2f4824412446ebb927843a05a9
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-06-27 10:09:55 -0700

    doing this stuff

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_line - draws a straight line in the terminal
+* @n: number of times the character '_' should be printed
+* Description: If n is 0 or less, the function prints only '\n'. Otherwise,
+* it prints n underscores followed by a newline character '\n'.
+*/
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (int i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
