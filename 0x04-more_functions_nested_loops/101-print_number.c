56d60c080a0a8aad26188c856aaa26ad44684dbc
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-06-27 10:57:23 -0700

    i aint pushing none after this

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* print_number - Prints an integer
-* @n: The integer to print
+*print_number - Prints an integer
+*@n: The integer to print
 */
 void print_number(int n)
 {
