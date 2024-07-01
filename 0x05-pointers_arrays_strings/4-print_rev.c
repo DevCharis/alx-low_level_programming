800898d32e6da5dfecd37b58b5c3f04564ec8162
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-07-01 03:28:22 -0700

    how was i supposed to know comments are different in c and c++

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,18 +9,15 @@ void print_rev(char *s)
 int len = 0;
 int i;
 
-// Calculate the length of the string
 while (s[len] != '\0')
 {
 len++;
 }
 
-// Print characters in reverse order
 for (i = len - 1; i >= 0; i--)
 {
 _putchar(s[i]);
 }
 
-// Print newline character
 _putchar('\n');
 }
