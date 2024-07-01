176da0cdc3884252c9076cd56b496ebc325234c9
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-07-01 04:14:11 -0700

    whats atoi?

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,25 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer
- * @s: the string to convert
- *
- * Return: the integer value of the string
+ * _atoi - converts a string to an integer.
+ * @s: the string to convert.
+ * Return: the integer value of the string.
  */
 int _atoi(char *s)
 {
-int i = 0, sign = 1;
-unsigned int result = 0;
-while (s[i] != '\0')
+int sign = 1;
+int result = 0;
+while (*s)
 {
-if (s[i] == '-')
-sign *= -1;
-else if (s[i] >= '0' && s[i] <= '9')
+if (*s == '-')
+sign = -sign;
+else if (*s >= '0' && *s <= '9')
 {
-if (result > (2147483647 - (s[i] - '0')) / 10)
-{
-result = (sign == 1) ? 2147483647 : 2147483648;
+result = result * 10 + (*s - '0');
+if (*(s + 1) < '0' || *(s + 1) > '9')
 break;
 }
-result = result * 10 + (s[i] - '0');
+s++;
 }
-else if (result > 0)
-break;
-i++;
-}
-return (sign == 1) ? result : -result;
+return (result *sign);
 }
