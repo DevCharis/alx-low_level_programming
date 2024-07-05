eff93583e10e503cf8fbfdb046636b491d44d06d
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-07-05 08:47:00 -0700

    done

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,47 +1,44 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * infinite_add - Adds two numbers stored as strings
- * @n1: The first number as a string
- * @n2: The second number as a string
- * @r: The buffer to store the result
- * @size_r: The size of the buffer
- *
- * Return: Pointer to the result, or 0 if result cannot be stored in r
- */
+* infinite_add - Adds two numbers represented as strings.
+* @n1: The first number string.
+* @n2: The second number string.
+* @r: The buffer to store the result.
+* @size_r: The size of the buffer.
+*
+* Return: A pointer to the result, or 0 if the result cannot be stored in r.
+*/
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0, carry = 0, sum = 0, max_len = 0;
-	int i, j;
+int i, j, k, len1, len2, sum, carry;
 
-	/* Calculate lengths of n1 and n2 */
-	while (n1[len1])
-		len1++;
-	while (n2[len2])
-		len2++;
+for (len1 = 0; n1[len1]; len1++);
+for (len2 = 0; n2[len2]; len2++);
+if (len1 >= size_r || len2 >= size_r)
+return (0);
 
-	/* Determine the maximum length of the result */
-	max_len = (len1 > len2) ? len1 : len2;
+carry = 0;
+for (i = len1 - 1, j = len2 - 1, k = 0; i >= 0 || j >= 0 || carry; i--, j--, k++)
+{
+if (k >= size_r - 1)
+return (0);
+sum = carry;
+if (i >= 0)
+sum += n1[i] - '0';
+if (j >= 0)
+sum += n2[j] - '0';
+r[k] = sum % 10 + '0';
+carry = sum / 10;
+}
+r[k] = '\0';
 
-	/* Check if size_r is sufficient to store the result */
-	if (max_len + 1 > size_r)
-	  return (0);
-
-	/* Start adding from the least significant digits */
-	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--)
-	{
-		sum = carry;
-		if (i >= 0)
-			sum += n1[i] - '0';
-		if (j >= 0)
-			sum += n2[j] - '0';
-
-		r[max_len--] = (sum % 10) + '0';
-		carry = sum / 10;
-	}
-
-	/* Null-terminate the result string */
-	r[len1 > len2 ? len1 : len2] = '\0';
-
-	return (r);
+for (i = 0, j = k - 1; i < j; i++, j--)
+{
+char temp = r[i];
+r[i] = r[j];
+r[j] = temp;
+}
+return (r);
 }
