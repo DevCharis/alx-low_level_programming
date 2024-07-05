2e4defdcb38f75ffc7f8400b11c8c5cd826e2ea8
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-07-05 08:31:33 -0700

    Its going pretty well, advanced stuff giving me a headache

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * infinite_add - Adds two numbers stored as strings
+ * @n1: The first number as a string
+ * @n2: The second number as a string
+ * @r: The buffer to store the result
+ * @size_r: The size of the buffer
+ *
+ * Return: Pointer to the result, or 0 if result cannot be stored in r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int len1 = 0, len2 = 0, carry = 0, sum = 0, max_len = 0;
+	int i, j;
+
+	/* Calculate lengths of n1 and n2 */
+	while (n1[len1])
+		len1++;
+	while (n2[len2])
+		len2++;
+
+	/* Determine the maximum length of the result */
+	max_len = (len1 > len2) ? len1 : len2;
+
+	/* Check if size_r is sufficient to store the result */
+	if (max_len + 1 > size_r)
+		return 0;
+
+	/* Start adding from the least significant digits */
+	for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--)
+	{
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
+
+		r[max_len--] = (sum % 10) + '0';
+		carry = sum / 10;
+	}
+
+	/* Null-terminate the result string */
+	r[len1 > len2 ? len1 : len2] = '\0';
+
+	return r;
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+
+void print_buffer(char *b, int size)
+{
+int offset, i;
+
+if (size <= 0)
+{
+putchar('\n');
+return;
+}
+
+for (offset = 0; offset < size; offset += 10)
+{
+printf("%08x: ", offset);
+
+for (i = 0; i < 10; i++)
+{
+if (i + offset < size)
+printf("%02x", b[offset + i]);
+else
+printf("  ");
+if (i % 2)
+putchar(' ');
+}
+
+for (i = 0; i < 10; i++)
+{
+if (i + offset < size)
+putchar(isprint(b[offset + i]) ? b[offset + i] : '.');
+}
+
+putchar('\n');
+}
+}
