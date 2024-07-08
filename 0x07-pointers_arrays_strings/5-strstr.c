32e1f123c4e5a9d8d7c2f073483c3042be1de2a7
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-07-08 07:30:44 -0700

    no idea what the probleme is

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,34 +1,30 @@
 #include "main.h"
+
 /**
  * _strstr - locates a substring
- * @haystack: the string to be scanned
- * @needle: the substring to be searched
+ * @haystack: the string to search in
+ * @needle: the substring to search for
  *
  * Return: a pointer to the beginning of the located substring,
  * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-char *h, *n;
+int i, j;
 
-while (*haystack != '\0')
-{
-h = haystack;
-n = needle;
+if (*needle == '\0')
+return (haystack);
 
-while (*n != '\0' && *haystack == *n)
+for (i = 0; haystack[i] != '\0'; i++)
 {
-haystack++;
-n++;
+for (j = 0; needle[j] != '\0'; j++)
+{
+if (haystack[i + j] != needle[j])
+break;
+}
+if (needle[j] == '\0')
+return (&haystack[i]);
 }
 
-if (*n == '\0')
-{
-return (h);
-}
-
-haystack = h + 1;
-}
-
-return (NULL);
+return (0);
 }
