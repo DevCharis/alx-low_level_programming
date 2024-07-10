83b4f43b3d5a62e81d8a9981791b9a77e1a5ed27
Joshua Bamidele <joshuabamidele9@gmail.com>
2024-07-10 03:06:44 -0700

    forgot the description

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include "main.h"
-
+/**
+ * _puts_recursion - Prints a string followed by a new line
+ * @s: The string to be printed
+ * Description: This function uses recursion to print each character
+ *              of the string until the null terminator is encountered.
+ *              After printing the last character, it prints a new line.
+ */
 void _puts_recursion(char *s)
 {
 if (*s == '\0')
