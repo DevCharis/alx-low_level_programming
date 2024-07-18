2eb7b172d8fdfdf429e3d6fc06e899fc5fc6a9d5
devcharis <joshuabamidele9@gmail.com>
2024-07-18 13:45:45 +0100

    trying linux local

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /* 
  * malloc_checked - Allocates memory using malloc
@@ -7,12 +8,11 @@
  * Return: A pointer to the allocated memory
  * If malloc fails, terminates the process with status value 98
  */
-
 void *malloc_checked(unsigned int b)
 {
 void *ptr = malloc(b);
 if (ptr == NULL)
 exit(98);
-return (ptr);
+return ptr;
 }
 
