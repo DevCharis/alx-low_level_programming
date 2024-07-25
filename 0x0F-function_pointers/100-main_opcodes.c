bcb0415a47e2e55ffe4e958d0e0fb9d0526f9ff6
devcharis <joshuabamidele9@gmail.com>
2024-07-25 04:52:30 +0100

    Advanced task(s)

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the opcodes of its own main function.
+ * @argc: The number of arguments.
+ * @argv: The array of arguments.
+ *
+ * Return: 0 on success, or an error code on failure.
+ */
+int main(int argc, char *argv[])
+{
+int bytes, i;
+char *main_ptr;
+
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
+
+bytes = atoi(argv[1]);
+if (bytes < 0)
+{
+printf("Error\n");
+return (2);
+}
+
+main_ptr = (char *)main;
+for (i = 0; i < bytes; i++)
+{
+printf("%02x", main_ptr[i] & 0xFF);
+if (i != bytes - 1)
+printf(" ");
+}
+printf("\n");
+
+return (0);
+}
+
