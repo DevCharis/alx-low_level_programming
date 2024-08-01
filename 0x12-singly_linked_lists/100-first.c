b49a68aceb2cf7829f03f1884728b4b15ced9429
devcharis <joshuabamidele9@gmail.com>
2024-08-01 14:12:25 +0100

    wetin be this advanced

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,15 +5,14 @@
  *
  * Description: This function is executed before the main function due to
  *              its use of the constructor attribute. It prints the following
- *              message: "You're beat! and yet,
- *              you must allow,\nI bore my house
+ *              message: "You're beat! and yet, you must allow,\n
+ *              I bore my house
  *              upon my back!\n".
  */
 void print_before_main(void) __attribute__ ((constructor));
 
 void print_before_main(void)
 {
-printf("You're beat! and yet, you must allow,\n
-		I bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
 
