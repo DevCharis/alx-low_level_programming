98da87780eec20a23366a0629bcfeebcd271483d
devcharis <joshuabamidele9@gmail.com>
2024-08-06 21:57:59 +0100

    mans gotta debug part 2

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -25,6 +25,22 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+
+
+int pop_listint(listint_t **head);
+
+int sum_listint(listint_t *head);
+
+
+size_t listint_len(const listint_t *h);
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
+
+
 
 #endif /* LISTS_H */
 
