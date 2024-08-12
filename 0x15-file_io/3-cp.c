da3978ff6ddb771df4daa2844064bddd78492de4
devcharis <joshuabamidele9@gmail.com>
2024-08-12 12:32:24 +0100

    some debugging part 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define BUF_SIZE 1024
+
+void check_arguments(int argc);
+int open_file_for_reading(const char *filename);
+int open_file_for_writing(const char *filename);
+void copy_content(int fd_from, int fd_to);
+void close_file(int fd);
+
 /**
  * main - Copies the content of a file to another file.
  * @argc: Number of arguments.
@@ -14,36 +22,90 @@
 int main(int argc, char *argv[])
 {
 int fd_from, fd_to;
-ssize_t bytes_read, bytes_written;
-char buffer[1024];
 
+check_arguments(argc);
+
+fd_from = open_file_for_reading(argv[1]);
+fd_to = open_file_for_writing(argv[2]);
+
+copy_content(fd_from, fd_to);
+
+close_file(fd_from);
+close_file(fd_to);
+
+return (0);
+}
+
+/**
+ * check_arguments - Checks if the number of arguments is correct.
+ * @argc: Number of arguments.
+ *
+ * If argc is not 3, prints the usage message and exits with code 97.
+ */
+void check_arguments(int argc)
+{
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
+}
 
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
+/**
+ * open_file_for_reading - Opens a file for reading.
+ * @filename: The name of the file to open.
+ *
+ * Return: The file descriptor of the opened file.
+ *         Exits with code 98 if the file cannot be read.
+ */
+int open_file_for_reading(const char *filename)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+int fd = open(filename, O_RDONLY);
+if (fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
 exit(98);
 }
-
-fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-if (fd_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(fd_from);
-exit(99);
+return (fd);
 }
 
-while ((bytes_read = read(fd_from, buffer, 1024)) > 0)
+/**
+ * open_file_for_writing - Opens a file for writing.
+ * @filename: The name of the file to open.
+ *
+ * Return: The file descriptor of the opened file.
+ *         Exits with code 99 if the file cannot be written to.
+ */
+int open_file_for_writing(const char *filename)
+{
+int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+exit(99);
+}
+return (fd);
+}
+
+/**
+ * copy_content - Copies the content from one file descriptor to another.
+ * @fd_from: The file descriptor to read from.
+ * @fd_to: The file descriptor to write to.
+ *
+ * Reads from fd_from and writes to fd_to in chunks of BUF_SIZE bytes.
+ * Exits with code 99 if writing fails or with code 98 if reading fails.
+ */
+void copy_content(int fd_from, int fd_to)
+{
+ssize_t bytes_read, bytes_written;
+char buffer[BUF_SIZE];
+
+while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
 {
 bytes_written = write(fd_to, buffer, bytes_read);
 if (bytes_written != bytes_read)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", buffer);
 close(fd_from);
 close(fd_to);
 exit(99);
@@ -52,22 +114,23 @@ exit(99);
 
 if (bytes_read == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file\n");
 exit(98);
 }
-
-if (close(fd_from) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-exit(100);
 }
 
-if (close(fd_to) == -1)
+/**
+ * close_file - Closes a file descriptor.
+ * @fd: The file descriptor to close.
+ *
+ * Exits with code 100 if closing the file descriptor fails.
+ */
+void close_file(int fd)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+if (close(fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 exit(100);
 }
-
-return (0);
 }
 
